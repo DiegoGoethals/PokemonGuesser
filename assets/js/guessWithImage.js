6b27471f2b1335406a64195d9faaf5bd722ff87b
diego <goethalsdiego@gmail.com>
2023-02-26 22:24:30 +0100

    Added custom pokemon font and background like the old school who's that pokemon

--- assets/js/guessWithImage.js
+++ assets/js/guessWithImage.js
@@ -1,7 +1,9 @@
 "use strict";
 
 function showImage(pokemon) {
-    document.querySelector("main").insertAdjacentHTML("afterbegin", `
-        <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="Sorry image not loading">`
+    document.querySelector("main").insertAdjacentHTML("beforeend", `
+        <div id="imageContainer">
+            <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="Sorry image not loading">
+        </div>`
     );
 }
