11180816536ee9cf5bd0db3eb0f88ca6a9b5ba42
diego <goethalsdiego@gmail.com>
2023-03-28 15:42:36 +0200

    Made it possible to see what you guessed, now only need to fix a bug where you can't see what guess was wrong and what was right

--- assets/js/guessWithImage.js
+++ assets/js/guessWithImage.js
@@ -1,9 +1,7 @@
 "use strict";
 
 function showImage(pokemon) {
-    document.querySelector("main").insertAdjacentHTML("beforeend", `
-        <div id="imageContainer">
-            <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="Sorry image not loading">
-        </div>`
+    document.querySelector("#imageContainer").insertAdjacentHTML("beforeend", `
+            <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="Sorry image not loading">`
     );
 }
--- assets/js/pokemonGuesser.js
+++ assets/js/pokemonGuesser.js
@@ -2,6 +2,7 @@
 
 init();
 let _pokemon;
+let _guesses_left = 6;
 
 function init() {
     setPokemon();
@@ -53,15 +54,70 @@ function autoComplete(e) {
     input.innerHTML = e.target.innerHTML;
     list.style.display = "none";
     guess();
+    input.value = "";
+    input.innerHTML = "";
 }
 
 function guess() {
     const guess = document.querySelector("input").value.toLowerCase();
+    showGuess(guess);
     if (guess === _pokemon.name) {
         alert("Congrats, you won");
         location.reload();
+    } else if (_guesses_left > 1) {
+        _guesses_left--;
     } else {
         alert("You lost, you big loser");
         location.reload();
     }
+}
+
+function showGuess(guess) {
+    fetchFromServer(`https://pokeapi.co/api/v2/pokemon/${guess}`, "GET").then(pokemon => {
+        document.querySelector("table").insertAdjacentHTML("beforeend", `
+        <tr>
+            <td>${guess.toUpperCase()}</td>
+            <td>${pokemon.id}</td>
+            ${getTypes(pokemon)}
+            ${getAbilities(pokemon)}
+        </tr>
+    `);
+    });
+}
+
+function getTypes(pokemon) {
+    let types = "";
+    console.log(_pokemon.types);
+    console.log(pokemon.types);
+    if (pokemon.types.length === 1) {
+        if (_pokemon.types.includes(pokemon.types[0])) {
+            types += `<td class="correct">${pokemon.types[0].type.name}</td><td>This pokemon only has 1 type</td>`
+        } else {
+            types += `<td>${pokemon.types[0].type.name}</td><td>This pokemon only has 1 type</td>`
+        }
+    } else {
+        pokemon.types.forEach(type => {
+            if (_pokemon.types.includes(type)) {
+                types += `<td class="correct">${type.type.name}</td>`;
+            } else {
+                types += `<td>${type.type.name}</td>`;
+            }
+        });
+    }
+    return types;
+}
+
+function getAbilities(pokemon) {
+    let abilities = "";
+    pokemon.abilities.forEach(ability => {
+        if (_pokemon.abilities.includes(ability)) {
+            abilities += `<td class="correct">${ability.ability.name}</td>`;
+        } else  {
+            abilities += `<td>${ability.ability.name}</td>`;
+        }
+    });
+    if (pokemon.abilities.length === 2) {
+        abilities += `<td>This pokemon only has 2 abilities</td>`
+    }
+    return abilities;
 }
